91bbb38ea0a194bb2ba8d13eada7069f7019e741
Darshan Chobarkar <dchobarkar@gmail.com>
2024-01-16 17:55:50 +0530

    add styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import "./App.css";
+
 function App() {
   const [rates, setRates] = useState();
   const [ratesFetched, setRatesFetched] = useState(false);
