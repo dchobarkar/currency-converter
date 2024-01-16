adb8934123729382e50aa7ea09aaa95d9f08a7ca
Darshan Chobarkar <dchobarkar@gmail.com>
2024-01-16 18:09:20 +0530

    add calculation logic

--- src/App.js
+++ src/App.js
@@ -5,31 +5,58 @@ import "./App.css";
 function App() {
   const [rates, setRates] = useState();
   const [ratesFetched, setRatesFetched] = useState(false);
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [output, setOutput] = useState();
+
+  useEffect(() => {
+    getRates();
+  }, []);
 
   const getRates = async () => {
+    // Fetch the data from API
     const response = await fetch(
       "https://v6.exchangerate-api.com/v6/980210ba6fb6b4867d436c88/latest/USD"
-    ).then((response) => response.json);
+    ).then((response) => response.json());
 
+    // Save the rates in the state
     if (response.resulte === "success") {
       setRates(response.conversion_rates);
       setRatesFetched(true);
     }
   };
 
-  useEffect(() => {
-    getRates();
-  }, []);
+  const calculateOutput = async () => {
+    // Fetch the selected from currency rates
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/980210ba6fb6b4867d436c88/latest/${fromCurrency}`
+    ).then((response) => response.json());
+
+    const fetchedRates = response.conversion_rates;
+    const currencyRate = fetchedRates[toCurrency];
+    const output = amount * currencyRate;
+    setOutput(output);
+  };
 
   return (
     <div className="container">
       <div className="input-amount">
         <label>Amount:</label>
-        <input type="number" id="amount" />
+        <input
+          type="number"
+          id="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
       </div>
       <div className="input-from">
         <label>From:</label>
-        <select id="from">
+        <select
+          id="from"
+          value={fromCurrency}
+          onChange={(e) => setFromCurrency(e.target.value)}
+        >
           {ratesFetched ? (
             Object.keys(rates).map((currency, index) => (
               <option key={index} value={currency}>
@@ -43,7 +70,11 @@ function App() {
       </div>
       <div className="input-to">
         <label>To:</label>
-        <select id="to">
+        <select
+          id="to"
+          value={toCurrency}
+          onChange={(e) => setToCurrency(e.target.value)}
+        >
           {ratesFetched ? (
             Object.keys(rates).map((currency, index) => (
               <option key={index} value={currency}>
@@ -55,9 +86,11 @@ function App() {
           )}
         </select>
       </div>
-      <button className="btn">Calculate</button>
+      <button className="btn" onClick={() => calculateOutput()}>
+        Calculate
+      </button>
       <div className="output">
-        <label>Output:</label>
+        <label>Output: {output}</label>
       </div>
     </div>
   );
